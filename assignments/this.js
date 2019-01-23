b50e070189adbf9939a54aa734e8f06c0216cff4
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-23 13:53:54 +0100

    Complete challenges in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,57 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+/* The for principles of "this"; 
+In your own words, explain the four principle for the "this" keyword below:
+
+* 1. Global Binding - when we are in the global scope, the value of this will be windowconsole Object. That means, that is we 
+create function in global scope, const myFunction = function() {console.log(this)}, this will refer to window Object.
+
+* 2. Implicit Binding - The object that is standing before the dot is what this keyword will be bound to. Whenever a function is called by a preceding dot, the object left to dot is this. 
+
+* 3. New Binding - Whenever we use constructor function, this reffers to a specific instance of the object that is created and returned by he constructor function.
+
+* 4. Explicit Binding - When we call or apply method is used, we explicitly define this.
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 = Global Binding
+const myFunction = function() {
+	console.log(this.a);
+};
 
-// code example for Window Binding
+var a = 4;
+myFunction();
 
-// Principle 2
+// Principle 2 = Implicit Binding
+const john = {
+	name: "John",
+	greet: function(person) {
+		console.log("Hi " + person + ", my name is " + this.name);
+	}
+};
 
-// code example for Implicit Binding
+john.greet("Mark");
 
-// Principle 3
+// Principle 3 = New Binding
 
-// code example for New Binding
+function Plant(plantName, plantHeight) {
+	this.plantName = plantName;
+	this.plantHeight = plantHeight;
+}
 
-// Principle 4
+Plant.prototype.watterPlant = function() {
+	console.log(`${this.plantName} has been wattered!`);
+};
 
-// code example for Explicit Binding
+const cactus = new Plant("cactus", "50 cm");
+
+cactus.watterPlant();
+
+// Principle 4 = Ecplicit Binding
+function doIt() {
+	console.log(this);
+}
+
+const myObj = {
+	doIt: doIt
+};
+doIt.call({ name: "Jerry" });
